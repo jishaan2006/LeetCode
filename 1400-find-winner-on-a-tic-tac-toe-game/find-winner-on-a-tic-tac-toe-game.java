775216c7b5782365823ba7bcba1f15ab59d5877a
Jishaan Khatri <jishaan123465@gmail.com>
2026-04-07 14:52:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (71.47%) - LeetSync

--- 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.java
+++ 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.java
@@ -10,12 +10,6 @@ class Solution {
             }
 
         }
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
 
         char win='\0';
         for (int i = 0; i < 3; i++) {
