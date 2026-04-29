4e3a71e5058254a407d92f01878c06849eeee970
Jishaan Khatri <jishaan123465@gmail.com>
2026-04-29 14:36:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (98.88%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,10 +1,18 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int c=0;
-        int[] expected= heights.clone();
-        Arrays.sort(expected);
-        for(int i=0;i<heights.length;i++){
-            if(heights[i]!=expected[i]) c++;
+        int c = 0;
+        int[] freq = new int[101];
+        for (int n : heights) {
+            freq[n]++;
+        }
+        int curr_height = 1;
+
+        for (int i = 0; i < heights.length; i++) {
+            while (freq[curr_height] == 0) curr_height++;
+            if (heights[i] != curr_height) {
+                c++;
+            }
+            freq[curr_height]--;
         }
         return c;
     }
