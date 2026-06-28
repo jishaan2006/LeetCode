923391147aa5bbcdbb11cc223e7737fa7a21181b
Jishaan Khatri <jishaan123465@gmail.com>
2026-06-28 19:09:16 +0530

    Time: 7 ms (89.18%) | Memory: 46.2 MB (50.70%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,30 +1,23 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int[] freq=new int[26];
-        char[] arr=s.toCharArray();
-        int left=0;
+        int[] freq = new int[26];
+        char[] arr = s.toCharArray();
+        int left = 0;
 
-        int longest_length=0;
-        for(int right=0;right<s.length();right++){
+        int longest_length = 0;
+        int max_freq=0;
+        for (int right = 0; right < s.length(); right++) {
 
-            freq[arr[right]-'A']++;
+            freq[arr[right] - 'A']++;
 
-            int max_freq=0;
-            for(int i:freq){
-                max_freq=Math.max(max_freq,i);
-            }
+            max_freq = Math.max(max_freq, freq[arr[right] - 'A']);
 
-            while((right-left+1)-max_freq>k){
-                freq[arr[left]-'A']--;
+            while ((right - left + 1) - max_freq > k) {
+                freq[arr[left] - 'A']--;
                 left++;
-
-                max_freq=0;
-                for(int i:freq){
-                    max_freq=Math.max(max_freq,i);
-                }
             }
-            longest_length=Math.max(right-left+1,longest_length);
 
+            longest_length = Math.max(longest_length, right - left + 1);
         }
         return longest_length;
     }
