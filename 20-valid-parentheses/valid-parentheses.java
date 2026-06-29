ecfd90b839bb1a1ae136f3be449dfa023558703f
Jishaan Khatri <jishaan123465@gmail.com>
2026-06-29 17:26:06 +0530

    Time: 3 ms (87.34%) | Memory: 43.4 MB (40.54%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+
+                if (c == ')') {
+                    if (st.peek() != '(') {
+                        return false;
+                    } else {
+                        st.pop();
+                    }
+                } else if (c == '}') {
+                    if (st.peek() != '{') {
+                        return false;
+                    } else {
+                        st.pop();
+                    }
+                } else if (c == ']') {
+                    if (st.peek() != '[') {
+                        return false;
+                    } else {
+                        st.pop();
+                    }
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
